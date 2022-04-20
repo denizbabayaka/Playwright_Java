df1e1782b8ef86ab1af8f0d7d02e74da7a3797a5
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-20 14:34:47 -0400

    new files added

---
+++ PlaywrightSessions/src/main/java/Playwrighrtsessions/CodeGenerator.java
@@ -0,0 +1,49 @@
+package Playwrighrtsessions;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import java.util.*;
+
+public class CodeGenerator {
+	
+	
+
+	
+	  public static void main(String[] args) {
+	    try (Playwright playwright = Playwright.create()) {
+	      Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+	        .setHeadless(false));
+	      BrowserContext context = browser.newContext();
+
+	      // Open new page
+	      Page page = context.newPage();
+
+	      // Go to https://www.amazon.com/
+	      page.navigate("https://www.amazon.com/");
+
+	      // Click text=Best Sellers
+	      page.click("text=Best Sellers");
+	      // assert page.url().equals("https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers");
+
+	      // Click text=Hello, Sign in
+	      page.click("text=Hello, Sign in");
+	      // assert page.url().equals("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fbestsellers%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+
+	      // Click input[name="email"]
+	      page.click("input[name=\"email\"]");
+
+	      // Click input[type="submit"]
+	      page.click("input[type=\"submit\"]");
+
+	      // Click text=Create your Amazon account
+	      page.click("text=Create your Amazon account");
+	      // assert page.url().equals("https://www.amazon.com/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fbestsellers%2F%3Fref_%3Dnav_ya_signin&prevRID=YB3PTAGJY1JAF1V1NHZY&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+
+	      // Go to https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fbestsellers%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&
+	      page.navigate("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fbestsellers%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
+	    }
+	  }
+}
+	
+
+
---
+++ PlaywrightSessions/src/main/java/Playwrighrtsessions/TotalAmazonLinks.java
@@ -0,0 +1,24 @@
+package Playwrighrtsessions;
+
+import java.util.List;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright. Page;
+import com.microsoft.playwright.Playwright;
+
+
+
+public class TotalAmazonLinks {
+	
+	 public static void main(String[] args) {
+	
+	 Playwright playwright = Playwright.create();
+	 Browser browser = playwright.chromium().launch(new BrowserType. LaunchOptions().setHeadless (false));
+	 Page page =browser.newPage();
+	 page.navigate("https://www.amazon.com/");
+	 List<String> amazonLinks = page.locator ("a:has-text('Amazon')").allInnerTexts();
+	 amazonLinks.stream().forEach(e-> System.out.println(e));
+	 
+
+}
+}
