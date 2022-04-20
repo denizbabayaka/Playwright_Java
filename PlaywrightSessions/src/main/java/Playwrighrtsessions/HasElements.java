517952a77168b8293434d0789854987e69ae6a63
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-20 16:45:08 -0400

    new files added

--- PlaywrightSessions/src/main/java/Playwrighrtsessions/HasElements.java
+++ PlaywrightSessions/src/main/java/Playwrighrtsessions/HasElements.java
@@ -14,11 +14,25 @@ public class HasElements {
 	 Playwright playwright = Playwright.create();
 	 Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 	 Page page=browser.newPage();
-	 page.navigate("https://www.orangehrm.com/orangehrm-30-day-trial/");
+	 //page.navigate("https://www.orangehrm.com/orangehrm-30-day-trial/");
+	 page.navigate("https://www.amazon.com/");
 	 
 	 // in this locator we are going from selectdropdown to options element locator chain 
-	 Locator loc=page. locator("select#Form_submitForm_Country:has(option[value='India'])");
-	 loc.allInnerTexts().forEach(e-> System.out.println(e));
+	 //Locator loc=page. locator("select#Form_submitForm_Country:has(option[value='India'])");
+	 //loc.allInnerTexts().forEach(e-> System.out.println(e));
+	 
+	 
+	 Locator footerList = page. locator("div.navFooterLinkCol:has(a[href='https://www.amazon.jobs'])");
+	 footerList.allInnerTexts().forEach(e-> System.out.println(e));
+	 
+	 
 	 }
 
 }
+
+      // parent to child has method =>playwright.inspect("div.navFooterLinkCol:has(a[href='https://www.amazon.jobs'])")
+
+
+
+
+
