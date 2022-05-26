a5836a521827b42421338ae3b2195994ab73876c
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-05-25 22:11:44 -0400

    new files added

--- PlaywrightSessions/src/main/java/Playwrighrtsessions/DownloadFile.java
+++ PlaywrightSessions/src/main/java/Playwrighrtsessions/DownloadFile.java
@@ -1,7 +1,5 @@
 package Playwrighrtsessions;
 
-
-
 import java.nio.file.Paths;
 
 import com.microsoft.playwright.Browser;
@@ -9,46 +7,37 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Download;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-public class DownloadFile{
-	
-	public static void main(String[]args){
 
-		Playwright playwright=Playwright.create();
-        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        
-        Page page =browser.newPage();
-        page.navigate("https://chromedriver.storage.googleapis.com/index.html?path=102.0.5005.27/");
-        
-        //This download class gets all the properties of  this element 
-       Download download= page.waitForDownload(() -> {
-        page.click("a:text('chromedriver_mac64.zip')");
-        
-	});
-       
-       //download.cancel(); instead of a failure
-       System.out.println(download.failure());
-       
-       System.out.println(download.url());  
-       System.out.println(download.page().title());
-       
-       String path=download.path().toString();
-       System.out.println(path);
-       
-       //This will download to the project path 
-       download.saveAs(Paths.get("Naveen_chrome.zip"));
-       
-       System.out.println(download.suggestedFilename());
+public class DownloadFile {
 
+	public static void main(String[] args) {
 
-       
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
-       
-       
+		Page page = browser.newPage();
+		page.navigate("https://chromedriver.storage.googleapis.com/index.html?path=102.0.5005.27/");
 
-       
+		// This download class gets all the properties of this element an take care of
+		// the download
+		Download download = page.waitForDownload(() -> {
+			page.click("a:text('chromedriver_mac64.zip')");
 
-       
-       
-                  
-  }
+		});
+
+		// download.cancel(); instead of a failure
+		System.out.println(download.failure());
+
+		System.out.println(download.url());
+		System.out.println(download.page().title());
+
+		String path = download.path().toString();
+		System.out.println(path);
+
+		// This will download to the project path
+		download.saveAs(Paths.get("Naveen_chrome.zip"));
+
+		System.out.println(download.suggestedFilename());
+
+	}
 }
